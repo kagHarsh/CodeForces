0332f11a80e4e0085d709a323d023c3931ef932f
kagHarsh <harshkagh.com>
2023-10-19 10:39:03 +0530

    second code

---
+++ 514A_chewbaca_number.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int x;
+    cin>>x;
+    long long int ans = 0;
+    long long int cnt = 1;
+    while (x > 0) {
+        int n = x%10;
+        x = x/10;
+        if (x == 0 && n == 9) {
+            ans = cnt*n + ans;
+            cnt = cnt*10;
+        }
+        else if (n > 4) {
+            n = 9-n;
+            ans = cnt*n + ans;
+            cnt = cnt*10;
+        } 
+        else if (n <= 4) {
+            ans = cnt*n + ans;
+            cnt = cnt*10;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
